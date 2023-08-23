36b840378f57f369e94604cabff6669ca1999c11
TaN Taa <atan60896@gmail.com>
2023-08-23 12:03:32 +0500

    add new section(order)

--- src/components/Order/index.jsx
+++ src/components/Order/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import phone from '../../assets/Images/png/order_phone.png';
 import './style.css';
 
 const index = () => {
@@ -9,7 +11,14 @@ const index = () => {
           <div className="order_wrapper">
             <div className="order_items">
               <h2>Консультируем по горячей линии</h2>
+              <div className="order_items_btn">
+                <h1>8 800 250 59 32</h1>
+                <p>Звонок по России бесплатный</p>
 
+                <button>Заказать звонок</button>
+              </div>
+
+              <img src={phone} alt="Phone" />
             </div>
           </div>
         </div>
