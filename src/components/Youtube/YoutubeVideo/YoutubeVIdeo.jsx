cdea626e7d56488587ad03d7daa45596587032b2
tursunov4 <tursunovsh4@gmail.com>
2023-08-25 14:10:52 +0500

    add youtubevideo page

--- src/components/Youtube/YoutubeVideo/YoutubeVIdeo.jsx
+++ src/components/Youtube/YoutubeVideo/YoutubeVIdeo.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./style.css"
+import YoutubeModaltext from "../YoutubevideoModal/YoutubevideoModal"
+import Modal from '../../Modal/Modal'
 const YoutubeVIdeo = () => {
+   const [modal , setModal] = useState(false)
+
   return (
-    <div>YoutubeVIdeo</div>
+    <div className='youtubevideo-wrapper'>
+      <Modal modal={modal} setModal={setModal}>
+         <YoutubeModaltext/>
+      </Modal>
+     <div className="youtubevideo-wrapper__text">
+      <h4>Все этапы приготовления пива в одном аппарате!</h4>
+      <ul className="youtube-video__text-list">
+        <li className='youtube-video__text-listitem'>
+           <h5>1. Затирание</h5>
+           <p>Запрограммируйте пивоварню на конкретный рецепт или выберете ручной режим, засыпьте дроблёный солод, запустите процесс.</p>
+        </li>
+        <li className='youtube-video__text-listitem'>
+           <h5>2. Фильтрация</h5>
+           <p>Извлеките заторный бак и поставьте устройство на подставку. Промываем дробину водой, заранее нагретой до 80–90°.</p>
+        </li>
+        <li className='youtube-video__text-listitem'>
+           <h5>3. Варка</h5>
+           <p>Добавьте хмель для горечи в самом начале варки. Добавьте хмель для вкуса, когда прошло ¾ всего процесса. Добавьте хмель для аромата за 5–7 минут до конца варки.</p>
+        </li>
+        <li className='youtube-video__text-listitem'>
+           <h5>4. Охлаждение</h5>
+           <p>Запрограммируйте пивоварню на конкретный рецепт или выберете ручной режим, засыпьте дроблёный солод, запустите процесс.</p>
+        </li>
+      </ul>
+      <div className="youtubevideo__mashina">
+       <img src="" alt="" />
+               
+      </div>
+      <div onClick={()=>setModal(true)} className='youtubevideo__mashina-text'>Подробнее об этапах приготовления</div>
+     </div>
+     <div className="youtubevide-wrapper__videos">
+      <div className='youtubevideo-wrapper__video' ></div>
+      <div className='youtubevideo-wrapper__video'></div>
+     </div>
+     
+    </div>
   )
 }
 
