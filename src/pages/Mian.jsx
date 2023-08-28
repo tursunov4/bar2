a269ae34828c82d9c09e77e0a4be03a5a0a5510b
tursunov4 <tursunovsh4@gmail.com>
2023-08-28 12:30:05 +0500

    asfasf

--- src/pages/Mian.jsx
+++ src/pages/Mian.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import Bavariya from "../components/Bavariya/Bavariya";
 import Padarka from "../components/Padarka/Padarka";
-import Bvcards from "../components/Bvcards";
+import Bvcards from "../components/Bvcards/Bvcard";
 import Delivery from "../components/Delivery";
 import Pokupke from "../components/Pokupke/Pokupka";
 import Order from "../components/Order";
@@ -15,14 +15,6 @@ import Kanstruktor from "../components/Kasntruktor/Kanstruktor";
 import Manage from "./../components/Manage/Manage";
 import Zakajite from "./../components/Zakajite/Zakajite";
 import Yandexmap from "../components/Yandexmap/Yandexmap";
-
-import Modal from "./../components/Modal/Modal";
-import Headertop from "../components/Header/Headertop/Headertop";
-
-import OrderedMd from "../components/Header/Ordered Modal/OrderedMd";
-
-
-import ZakajiteModal from "../components/Zakajite/Zakajitemodal/ZakajiteModal";
 
 
 const Mian = () => {
